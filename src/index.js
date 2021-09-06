4da557d2924fc38617f8e0310d1a8875ca1e4248
RomanIlienko <75617889+RomanIlienko@users.noreply.github.com>
2021-09-06 15:15:32 +0300

    add markup

--- src/index.js
+++ src/index.js
@@ -6,3 +6,36 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('.start'),
+  stopBtn: document.querySelector('.stop')
+}
+
+refs.startBtn.addEventListener('click', switchColorStart)
+refs.stopBtn.addEventListener('click', switchColorStop)
+
+let timerId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function switchColorStart() {
+  timerId = setInterval(() => {
+
+   }, 1000);
+  refs.startBtn.setAttribute('disabled', true)
+  refs.body.style.backgroundColor = randomIntegerFromInterval
+}
+
+function switchColorStop() {
+  clearInterval(timerId);
+  refs.startBtn.removeAttribute('disabled', true)
+};
+
+// function setBodyColor() {
+//   refs.body.style.backgroundColor = 
+// }
+
